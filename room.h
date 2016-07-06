a2018549381225bd7b157f4a5d35236a75b377f3
222222day <222222day>
2016-07-05 23:31:37 -0500

    minor changes. unfinished

--- room.h
+++ room.h
@@ -20,15 +20,30 @@ namespace room {
                        BED_POS  = 2;
 
 
-    sf::Sprite* spr_p;
+    std::vector<sf::Sprite> spr_p;
     txmg::TexPtr tx_p [TX_P_SIZE];
 
     err::Error room_gui_load () {
-        spr_p = new sf::Sprite [3];
+        spr_p.resize(3);
 
+            // load sf::textures
         tx_p[0] = { txmg::makeTex( fp::room_i ) };
         tx_p[1] = { txmg::makeTex( fp::desk_i ) };
         tx_p[2] = { txmg::makeTex( fp::bed_i  ) };
+
+            // check for error
+        for (int n = 0; n < TX_P_SIZE; ++n) {
+            if ((sf::Texture*)tx_p[n] == nullptr) {
+                err::Error param;
+                param.setFile(__FILE__).setLine(__LINE__).setText("Error: loading tx_p");
+                return param;
+            }
+        }
+
+        // set textures into sprites
+        for (int n = 0; n < spr_p.size(); ++n) {
+            spr_p[n].setTexture(sf::Texture*(tx_p[n]));
+        }
     }
 
     void room_gui_close () {
